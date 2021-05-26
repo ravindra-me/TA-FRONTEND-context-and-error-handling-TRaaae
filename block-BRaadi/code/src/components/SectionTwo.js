a050454d92a224c652ad549b28be482f2fbdf20b
ravidra-me <ravindrarajpoot9628172@gmail.com>
2021-05-26 22:04:32 +0530

    error handling

--- block-BRaadi/code/src/components/SectionTwo.js
+++ block-BRaadi/code/src/components/SectionTwo.js
@@ -1,23 +1,28 @@
 import React from "react";
-
+import { Error } from "./App";
 class SectionTwo extends React.Component {
   handleClick = () => {
     throw new Error("An error occured!");
   };
   render() {
-    return (
-      <div className="box s1-c">
-        <h2>Throw Error</h2>
-        <p className="small">
-          Clicking this button will call a function. Inside that function an
-          error is thrown. That will make the component gets unmounted. Fix it
-          using error boundary. Make this component display `An error occured!`
-        </p>
-        <button className="error" onClick={this.handleClick}>
-          Click to Throw an Error
-        </button>
-      </div>
-    );
+    try {
+      return (
+        <div className="box s1-c">
+          <h2>Throw Error</h2>
+          <p className="small">
+            Clicking this button will call a function. Inside that function an
+            error is thrown. That will make the component gets unmounted. Fix it
+            using error boundary. Make this component display `An error
+            occured!`
+          </p>
+          <button className="error" onClick={this.handleClick}>
+            Click to Throw an Error
+          </button>
+        </div>
+      );
+    } catch (error) {
+      return console.log(error);
+    }
   }
 }
 
